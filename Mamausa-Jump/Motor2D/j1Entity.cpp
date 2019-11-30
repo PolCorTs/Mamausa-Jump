8d59fd628917bca7cd18b514271ba68fd4dba11f
Albertito029 <36154189+Albertito029@users.noreply.github.com>
2019-11-30 19:27:35 +0100

    Flip works

--- Mamausa-Jump/Motor2D/j1Entity.cpp
+++ Mamausa-Jump/Motor2D/j1Entity.cpp
@@ -8,14 +8,14 @@ j1Entity::j1Entity(int x, int y, ENTITY_TYPES type) : position(x, y), type(type)
 
 void j1Entity::Draw(SDL_Rect r, bool flip, float x, float y)
 {
-	/*if (flip == false)
+	if (flip == true)
 	{
-		App->render->Blit(sprites, (int)position.x + x, (int)position.y + y, &r, SDL_FLIP_NONE);
+		App->render->Blit(sprites, x, y, &r, SDL_FLIP_NONE);
 	}
 	else
 	{
-		App->render->Blit(sprites, (int)position.x + x, (int)position.y + y, &r, SDL_FLIP_HORIZONTAL);
-	}*/
+		App->render->Blit(sprites, x, y, &r, SDL_FLIP_HORIZONTAL);
+	}
 
-	App->render->Blit(sprites, (int)position.x + x, (int)position.y + y, &r, SDL_FLIP_NONE);
+	//App->render->Blit(sprites, position.x + x, position.y + y, &r, SDL_FLIP_NONE);
 }
